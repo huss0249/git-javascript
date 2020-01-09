6a49bac069a7ce3fbfc6e1fec662d376ee550938
Ahmed Hussein <huss0249@algonquinlive.com>
2020-01-09 17:52:06 -0500

    added comments to js file

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// Created the name variable
 const name = "Ahmed Hussein"
 console.log(`Hello, my name is ${name}`)
-
+/*
+    1) Created the number variable 7
+    2) 7 X 2 = 14
+    3) 14 X 5 = 70
+    4) 70 / 7 = 10
+    5) 10 - 7 = 3
+*/
 const number = 7
 console.log((((number * 2) * 5) / number) - 7 )
